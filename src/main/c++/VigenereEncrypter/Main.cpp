d3388ced847a9fd9b7416bf673af6f8a85ec4c39
Julian Neuberger <neuberger.julian@gmail.com>
2015-10-26 16:49:51 +0100

    Inital commit for Vigenere Chiffre

---
+++ src/main/c++/VigenereEncrypter/Main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "VignereEncrypter.h"
+using namespace std;
+
+int main(){
+	std::string in;
+	std::string key;
+	char whatToDo;
+	bool decided = false;
+	bool desicion;
+		
+	cout << "Enter the message to de/encrypt" << std::endl;
+	cin >> in;
+
+	cout << "Enter a key" << std::endl;
+	cin >> key;
+
+	VigenereEncrypter encrypter(&key);
+	std::string out;
+
+	cout << "Do you want to encrypt (e) or decrypt (d)?" << std::endl;
+	while (!decided) {
+		cin >> whatToDo;
+		if(whatToDo == 'e' || whatToDo == 'E') {
+			encrypter.encrypt(&in, &out);
+			decided = true;
+		} else if( whatToDo == 'd' || whatToDo == 'D') {
+			encrypter.decrypt(&in, &out);
+			decided = true;
+		} else {
+			cout << "Please enter either e or d!";
+		}
+	}
+	
+	cout << "Result: " << out << std::endl;
+
+	return 0;
+}
---
+++ src/main/c++/VigenereEncrypter/VigenereEncrypter.cpp
@@ -0,0 +1,55 @@
+#include "VignereEncrypter.h"
+
+VigenereEncrypter::VigenereEncrypter(std::string* key) : key(*key) {
+	this->keyLength = key->length();
+}
+
+void VigenereEncrypter::encrypt(std::string* in, std::string* encrypted) {
+	this->helper(in, encrypted, true);
+}
+
+void VigenereEncrypter::decrypt(std::string* in, std::string* decrypted) {
+	this->helper(in, decrypted, false);
+}
+
+void VigenereEncrypter::helper(std::string* in, std::string* out, bool encrypt) {
+	int index = 0;
+	int keyIndex;
+	int keyChar;
+	char encodedChar;
+	int curValue;
+	int encodedValue;
+	for(std::string::iterator iter = in->begin(); iter != in->end(); ++iter) {
+		keyIndex = index % (this->keyLength);
+		keyChar = ((int) this->key.at(keyIndex)) - VigenereEncrypter::ALPHABET_OFFSET;
+		if(*iter == '_') {
+			curValue = VigenereEncrypter::SPACE_VALUE;
+		} else {
+			curValue = (int) *iter;
+		}
+		curValue -= VigenereEncrypter::ALPHABET_OFFSET;
+
+		int tmp;
+		if(encrypt) {
+			tmp = curValue + keyChar;
+		} else {
+			tmp = curValue - keyChar;
+		}
+		tmp = tmp % VigenereEncrypter::ALPHABET_SIZE;
+		if(tmp < 0) {
+			tmp = VigenereEncrypter::ALPHABET_SIZE + tmp;
+		}
+		//std::cout << tmp << "|";
+
+		encodedValue = tmp + VigenereEncrypter::ALPHABET_OFFSET;
+		
+		if(encodedValue == VigenereEncrypter::SPACE_VALUE) {
+			encodedChar = '_';
+		} else {
+			encodedChar = (char) encodedValue;
+		}
+
+		*out += encodedChar;
+		++index;
+	}
+}
